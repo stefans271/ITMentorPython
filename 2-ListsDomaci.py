e5dbfe7a1a4c69cc8338d73bf738f58459b45f9e
stefans271 <stefansavicmtg@yahoo.com>
2025-12-17 20:27:39 +0100

    2-cas-Lists-homework

---
+++ 2-ListsDomaci.py
@@ -0,0 +1,15 @@
+person = {
+    "person1":{
+        "name":"Toma",
+        "last_name":"Nikolic"
+    },
+    "person2":{
+        "name":"Petar",
+        "last_name":"Pavlovic"
+    },
+    "person3":{
+        "name":"Mihajlo",
+        "last_name":"Folic"
+    }
+}
+print(person["person2"]["name"],person["person3"]["last_name"])
